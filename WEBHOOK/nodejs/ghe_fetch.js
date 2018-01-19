c02f7eaa0687464bb3c7833298ebdbe7cd3f1dbd
Nathan Pearce <nathan.pearce@gmail.com>
2018-01-19 11:53:04 -0800

    ghe_fetch - retrieves 'commit' file attributes and Service Definitions.

--- WEBHOOK/nodejs/ghe_fetch.js
+++ WEBHOOK/nodejs/ghe_fetch.js
@@ -2,12 +2,90 @@ var logger = require('f5-logger').getInstance();
 var http = require('https');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
+// TODO Support YAML & JSON? iImplement 'try { JSON.parse }' else check if YAML...
+
 function GheFetch() {}
 
 /**
  * Fetches data from GitHub Enterprise
  */
-GheFetch.getServiceDefinition = function (GHE_IP_ADDR, GHE_ACCESS_TOKEN, pathToData, cb) {
+GheFetch.getAddedServiceDefinition = function (GHE_IP_ADDR, GHE_ACCESS_TOKEN, addedAppServicePath) {
+
+  GheFetch.getGheDownloadUrl(GHE_IP_ADDR, GHE_ACCESS_TOKEN, addedAppServicePath, function (download_url) {
+
+    logger.info("GheFetch.getGheDownloadUrl fetched URL: " +download_url+ "\n Fetching Added Service Definition...");
+
+    var options = {
+      "method": "GET",
+      "hostname": GHE_IP_ADDR,
+      "port": 443,
+      "path": download_url,
+      "headers": {
+        "cache-control": "no-cache",
+        "authorization": "Bearer " +GHE_ACCESS_TOKEN
+      }
+    };
+
+    var req = http.request(options, function (res) {
+      var chunks = [];
+      res.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
+      res.on("end", function () {
+        var body = Buffer.concat(chunks);
+        results = body.toString();
+        logger.info("GheFetch.getAddedServiceDefinition() - return results: " +results);
+      });
+    }).on("error", function (err) {
+      logger.info("GheFetch.getServiceDefinition: Error: " +err);
+    });
+    req.end();
+
+  });
+
+}
+
+GheFetch.getGheDownloadUrl = function(GHE_IP_ADDR, GHE_ACCESS_TOKEN, addedAppServicePath, download_url) {
+
+  logger.info("GheFetch.getGheDownloadUrl() fetching " +addedAppServicePath+ " from: " +GHE_IP_ADDR);
+
+  var options = {
+    "method": "GET",
+    "hostname": GHE_IP_ADDR,
+    "port": 443,
+    "path": addedAppServicePath,
+    "headers": {
+      "cache-control": "no-cache",
+      "authorization": "Bearer " +GHE_ACCESS_TOKEN
+    }
+  };
+
+  var req = http.request(options, function (res) {
+    var chunks = [];
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+    res.on("end", function () {
+      var body = Buffer.concat(chunks);
+      results = body.toString();
+
+//      logger.info("GheFetch.getGheDownloadUrl() - return results: " +JSON.stringify(JSON.parse(results), '', '\t'));
+      let parsed_results = JSON.parse(results);
+
+      logger.info("GheFetch.getGheDownloadUrl() - parsed_results.download_url " +parsed_results.download_url);
+
+      download_url(parsed_results.download_url);
+
+    });
+  }).on("error", function (err) {
+    logger.info("GheFetch.getServiceDefinition: Error: " +err);
+  });
+  req.end();
+
+}
+
+//TODO phone home for the WebHook State....
+GheFetch.getState = function (GHE_IP_ADDR, GHE_ACCESS_TOKEN, pathToData, cb) {
 
   logger.info("GheFetch.getServiceDefinition() fetching " +pathToData+ " from: " +GHE_IP_ADDR);
 
@@ -39,5 +117,6 @@ GheFetch.getServiceDefinition = function (GHE_IP_ADDR, GHE_ACCESS_TOKEN, pathToD
   req.end();
 
 }
+
 
 module.exports = GheFetch;
