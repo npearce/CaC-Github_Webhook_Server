213bd87173de4919a59565b01247cc11e40a87fb
Nathan Pearce <nathan.pearce@gmail.com>
2018-01-19 11:53:45 -0800

    Wrapper to handle Device Definitions (on-boarding)

--- WEBHOOK/nodejs/ghe_onboard.js
+++ WEBHOOK/nodejs/ghe_onboard.js
@@ -2,14 +2,14 @@ var logger = require('f5-logger').getInstance();
 var http = require('http');
 
 // POST the device phone-home details to this worker
-// Worker will then retrieve its "on-boarding" details from the repo and execute the Onboarding worker.
+// Worker will then retrieve its "on-boarding" details from the repo and execute the Device Onboarding worker.
 
 function GheOnBoard() {}
 
-GheListener.prototype.WORKER_URI_PATH = "shared/iac/ghe_onboard";
-GheListener.prototype.isPublic = true;
+GheOnBoard.prototype.WORKER_URI_PATH = "shared/iac/ghe_onboard";
+GheOnBoard.prototype.isPublic = true;
 
-GheListener.prototype.onStart = function(success, error) {
+GheOnBoard.prototype.onStart = function(success, error) {
 
   logger.info("GitHub Enterprise WebHook Server: onStart()...");
 
@@ -19,4 +19,6 @@ GheListener.prototype.onStart = function(success, error) {
 // this should actually check that the Device Reset/Onbaord worker is installed and loaded `/available` before 'success();'.
   success();
 
-}
+};
+
+module.exports = GheOnBoard;
