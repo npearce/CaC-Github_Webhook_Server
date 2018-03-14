a3545510b70a6c3a0ab756ba2b39514d88584f63
Nathan Pearce <nathan.pearce@gmail.com>
2018-03-13 21:44:28 -0700

    Added Support for persisted state (survives reboot).

--- WEBHOOK/nodejs/ghe_listener.js
+++ WEBHOOK/nodejs/ghe_listener.js
@@ -8,19 +8,45 @@
 */
 "use strict";
 
-var logger = require('f5-logger').getInstance();
-var http = require('http');
-var ServiceAction = require('./as3_service_action.js');
+const logger = require('f5-logger').getInstance();
+const http = require('http');
+const ServiceAction = require('./as3_service_action.js');
 
-function GheListener() {}
+//  const GHE_IP_ADDR = '172.31.1.200';
+//  const GHE_ACCESS_TOKEN = '83037830f391bf6ac26482332b92ae36e2da4457';
+
+function GheListener() {
+  this.state = {};
+}
 
 GheListener.prototype.WORKER_URI_PATH = "shared/n8/ghe_listener";
 GheListener.prototype.isPublic = true;
+GheListener.prototype.isPersisted = true;
+GheListener.prototype.isSingleton = true;
 
+var GHE_IP_ADDR = "";
+var GHE_ACCESS_TOKEN = ""; 
+
+/**
+ * handle onStart
+ */
 GheListener.prototype.onStart = function(success, error) {
 
-  logger.info("GitHub Enterprise WebHook Server Listener: onStart()...");
+  logger.info("[GheListener] GitHub Enterprise WebHook Server Listener: onStart()...");
 
+  var that = this;  
+  this.loadState(null, function (err, state) {
+    if (err) {
+      error('Error loading state: ' +err);
+    }
+    else {
+      logger.info("[GheListener] The state is: " +JSON.stringify(state));
+      if (state.config) {
+        that.state = state;
+        success('[GheListener] State loaded...');
+      }
+    }
+  });
 };
 
 /**
@@ -38,59 +64,64 @@ GheListener.prototype.onGet = function(restOperation) {
  */
 GheListener.prototype.onPost = function(restOperation) {
 
-  var gheMessage = restOperation.getBody();
-  logger.info("Activity from repository: " +gheMessage.repository.name);
+  //TODO: Move these into GheFetch - call them via 'GreetngWorker.js' model.
+  var GHE_IP_ADDR = this.state.config.ghe_ip_address;
+  var GHE_ACCESS_TOKEN = this.state.config.ghe_access_token; 
 
-  // Iterate through commit messages to handle added|modified|removed definitions
-  for (var i in gheMessage.commits) {
-
-    // Handle new device/service definitions.
-    if (gheMessage.commits[i].added.length > 0) {
-
-      var addedFile = gheMessage.commits[i].added.toString();
-      var addedFilePath = "/api/v3/repos/"+gheMessage.repository.full_name+"/contents/"+addedFile;
-
-      if (addedFile.startsWith("SERVICE")) {
-        logger.info("This is a 'SERVICE' definition: " +addedFile);  
-        ServiceAction.deploy(GHE_IP_ADDR, GHE_ACCESS_TOKEN, addedFilePath);
-      }
-    }
-
-    // Handle modified device/service definitions.
-    if (gheMessage.commits[i].modified.length > 0) {
-
-      var modifiedFile = gheMessage.commits[i].modified.toString();
-      var modifiedFilePath = "/api/v3/repos/"+gheMessage.repository.full_name+"/contents/"+modifiedFile;
-
-      if (modifiedFile.startsWith("SERVICE")) {
-        logger.info("This is a 'SERVICE' definition: " +modifiedFile);
-        ServiceAction.modify(GHE_IP_ADDR, GHE_ACCESS_TOKEN, modifiedFilePath);
-      }
-    }
-
-    // Handle deleted device/service definitions.
-    if (gheMessage.commits[i].removed.length > 0)  {
-
-      // The definition has been deleted, so we must retrieve it from the previous commit - 'gheMessage.before'.
-      var previousCommit = gheMessage.before;
-
-      var deletedFile = gheMessage.commits[i].removed.toString();
-      var deletedFilePath = "/api/v3/repos/"+gheMessage.repository.full_name+"/contents/"+deletedFile+"?ref="+previousCommit;
-
-      if (deletedFile.startsWith("SERVICE")) {
-        logger.info("This is a 'SERVICE' definition: " +deletedFile);
-
-        // Hand off to GheFetch Service Definition from GitHub enterprise
-        ServiceAction.delete(GHE_IP_ADDR, GHE_ACCESS_TOKEN, deletedFilePath);
-      }
-    }
+  if (!GHE_IP_ADDR || !GHE_ACCESS_TOKEN) {
+    console.log('[GheListener] Requires GHE_IP_ADDR & !GHE_ACCESS_TOKEN to function.');
+    restOperation.setBody('Requires GHE_IP_ADDR & !GHE_ACCESS_TOKEN to function.');
+    this.completeRestOperation(restOperation);
   }
+  else {
+    var gheMessage = restOperation.getBody();
+    logger.info("[GheListener] Activity from repository: " + gheMessage.repository.name);
 
-// Respond to GHE WebHook Client
-  restOperation.setBody("Thanks, GitHub Enterprise!");
-  restOperation.setStatusCode('200');
-  restOperation.setContentType('text');
-  this.completeRestOperation(restOperation);
+    // Iterate through commit messages to handle added|modified|removed definitions
+    for (var i in gheMessage.commits) {
+
+      // Handle new device/service definitions.
+      if (gheMessage.commits[i].added.length > 0) {
+
+        var addedFile = gheMessage.commits[i].added.toString();
+        var addedFilePath = "/api/v3/repos/" + gheMessage.repository.full_name + "/contents/" + addedFile;
+
+        if (addedFile.startsWith("SERVICE")) {
+          ServiceAction.deploy(GHE_IP_ADDR, GHE_ACCESS_TOKEN, addedFilePath);
+        }
+      }
+
+      // Handle modified device/service definitions.
+      if (gheMessage.commits[i].modified.length > 0) {
+
+        var modifiedFile = gheMessage.commits[i].modified.toString();
+        var modifiedFilePath = "/api/v3/repos/" + gheMessage.repository.full_name + "/contents/" + modifiedFile;
+
+        if (modifiedFile.startsWith("SERVICE")) {
+          ServiceAction.modify(GHE_IP_ADDR, GHE_ACCESS_TOKEN, modifiedFilePath);
+        }
+      }
+
+      // Handle deleted device/service definitions.
+      if (gheMessage.commits[i].removed.length > 0) {
+
+        // The definition has been deleted, so we must retrieve it from the previous commit - 'gheMessage.before'.
+        var previousCommit = gheMessage.before;
+        var deletedFile = gheMessage.commits[i].removed.toString();
+        var deletedFilePath = "/api/v3/repos/" + gheMessage.repository.full_name + "/contents/" + deletedFile + "?ref=" + previousCommit;
+
+        if (deletedFile.startsWith("SERVICE")) {
+          ServiceAction.delete(GHE_IP_ADDR, GHE_ACCESS_TOKEN, deletedFilePath);
+        }
+      }
+    }
+
+    // Respond to GHE WebHook Client
+    restOperation.setBody("[GheListener] Thanks, GitHub Enterprise!");
+    restOperation.setStatusCode('200');
+    restOperation.setContentType('text');
+    this.completeRestOperation(restOperation);
+  }
 };
 
 /**
@@ -99,12 +130,7 @@ GheListener.prototype.onPost = function(restOperation) {
 GheListener.prototype.onPut = function(restOperation) {
 
   var newState = restOperation.getBody();
-  this.state.GHE_IP_ADDR = newState.ghe_ip_address;
-  this.state.GHE_ACCESS_TOKEN = newState.ghe_access_token;
   this.state = newState;
-  
-//  var GHE_IP_ADDR = '172.31.1.200';
-//  var GHE_ACCESS_TOKEN = '83037830f391bf6ac26482332b92ae36e2da4457';
 
   restOperation.setBody(this.state);
   this.completeRestOperation(restOperation);
@@ -116,9 +142,9 @@ GheListener.prototype.onPut = function(restOperation) {
  */
 GheListener.prototype.getExampleState = function () {
   return {
-    "ghe_ip_address":"x.x.x.x",
-    "ghe_access_token": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-  }
+    "ghe_ip_address":"[ip_address]",
+    "ghe_access_token": "[GitHub Access Token]"
+  };
 };
 
 module.exports = GheListener;
